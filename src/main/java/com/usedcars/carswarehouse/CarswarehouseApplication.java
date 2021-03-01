4c9a42547ba79ec087f2d4be5ff1336c9afb4d13
jayarajsm <jayarajsm@gmail.com>
2021-03-01 10:35:16 +0100

    Initial version

---
+++ src/main/java/com/usedcars/carswarehouse/CarswarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.usedcars.carswarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarswarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarswarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usedcars/carswarehouse/CarswarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usedcars.carswarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarswarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
